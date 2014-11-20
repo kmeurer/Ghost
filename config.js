484cf5b0e713bc84f1a197a7a21b7ff58b2a794e
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-20 09:36:38 -0800

    Modify config.js for different url.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://kmeurer.azurewebsites.net/',
+        url: 'http://kevinmeurer.com/',
         mail: {},
         database: {
             client: 'sqlite3',
